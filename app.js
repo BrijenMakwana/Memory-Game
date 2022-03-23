8731598b117e1314d2ade16807104e46704b18ea
Brijen Makwana <brijenmakwana@Brijens-MacBook-Air.local>
2022-03-23 10:56:40 +0530

    Game logic implemented

--- app.js
+++ app.js
@@ -1,66 +1,115 @@
 const cardArray = [
   {
     name: "cheeseburger",
-    image: "images/cheeseburger.png",
+    image:
+      "https://cdn.pixabay.com/photo/2012/04/13/01/51/hamburger-31775_960_720.png",
   },
   {
     name: "fries",
-    image: "images/fries.png",
+    image:
+      "https://cdn.pixabay.com/photo/2013/07/13/01/24/french-fries-155679_960_720.png",
   },
   {
     name: "hotdog",
-    image: "images/hotdog.png",
+    image:
+      "https://cdn.pixabay.com/photo/2012/04/26/10/44/hotdog-42015_960_720.png",
   },
   {
     name: "ice-cream",
-    image: "images/ice-cream.png",
+    image:
+      "https://cdn.pixabay.com/photo/2016/06/02/22/14/ice-1432278_960_720.png",
   },
   {
     name: "milkshake",
-    image: "images/milkshake.png",
+    image:
+      "https://cdn.pixabay.com/photo/2012/04/14/15/57/drinks-34377_960_720.png",
   },
   {
     name: "pizza",
-    image: "images/pizza.png",
+    image:
+      "https://cdn.pixabay.com/photo/2016/06/01/12/59/pizza-1428931_960_720.png",
   },
   {
     name: "cheeseburger",
-    image: "images/cheeseburger.png",
+    image:
+      "https://cdn.pixabay.com/photo/2012/04/13/01/51/hamburger-31775_960_720.png",
   },
   {
     name: "fries",
-    image: "images/fries.png",
+    image:
+      "https://cdn.pixabay.com/photo/2013/07/13/01/24/french-fries-155679_960_720.png",
   },
   {
     name: "hotdog",
-    image: "images/hotdog.png",
+    image:
+      "https://cdn.pixabay.com/photo/2012/04/26/10/44/hotdog-42015_960_720.png",
   },
   {
     name: "ice-cream",
-    image: "images/ice-cream.png",
+    image:
+      "https://cdn.pixabay.com/photo/2016/06/02/22/14/ice-1432278_960_720.png",
   },
   {
     name: "milkshake",
-    image: "images/milkshake.png",
+    image:
+      "https://cdn.pixabay.com/photo/2012/04/14/15/57/drinks-34377_960_720.png",
   },
   {
     name: "pizza",
-    image: "images/pizza.png",
+    image:
+      "https://cdn.pixabay.com/photo/2016/06/01/12/59/pizza-1428931_960_720.png",
   },
 ];
 
 cardArray.sort(() => 0.5 - Math.random());
+let cardsChosen = [];
+let cardChosenIds = [];
 
-const score = document.getElementById("score");
 const grid = document.querySelector("#grid");
 
+// add images to the grid
 function createBoard() {
   for (let i = 0; i < cardArray.length; i++) {
     const card = document.createElement("img");
     card.setAttribute("src", "images/blank.png");
-    card.setAttribute("id", i);
+    card.setAttribute("data-id", i);
+    card.addEventListener("click", flipCard);
     grid.appendChild(card);
   }
 }
 
 createBoard();
+
+// check for the match
+function checkMatch() {
+  const cards = document.querySelectorAll("img");
+
+  if (cardChosenIds[0] === cardChosenIds[1]) {
+    alert("you clicked the same image");
+    cards[cardChosenIds[0]].setAttribute("src", "images/blank.png");
+    cards[cardChosenIds[1]].setAttribute("src", "images/blank.png");
+  } else if (cardsChosen[0] === cardsChosen[1]) {
+    // alert("you found a match");
+    cards[cardChosenIds[0]].setAttribute("src", "images/white.png");
+    cards[cardChosenIds[1]].setAttribute("src", "images/white.png");
+    cards[cardChosenIds[0]].removeEventListener("click", flipCard);
+    cards[cardChosenIds[1]].removeEventListener("click", flipCard);
+  } else {
+    cards[cardChosenIds[0]].setAttribute("src", "images/blank.png");
+    cards[cardChosenIds[1]].setAttribute("src", "images/blank.png");
+  }
+  cardsChosen = [];
+  cardChosenIds = [];
+}
+
+// reveal the image
+function flipCard() {
+  const cardId = this.getAttribute("data-id");
+  cardsChosen.push(cardArray[cardId].name);
+  cardChosenIds.push(cardId);
+  this.setAttribute("src", cardArray[cardId].image);
+
+  if (cardsChosen.length === 2) {
+    setTimeout(checkMatch, 500);
+  }
+}
